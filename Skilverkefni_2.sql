b3b404451c87638664d3b0ae8794a3ac79bcc2ed
Frillion <fridirksoebech@gmail.com>
2019-09-27 14:48:23 +0000

    first commit one code issue left

--- Skilverkefni_2.sql
+++ Skilverkefni_2.sql
@@ -14,13 +14,28 @@ before insert on restrictors
 for each row
 begin
 	declare msg varchar(255);
-    if(new.)
+    if(new.courseNumber = new.restrictorID)
+		then set msg = concat("Það er ekki hægt að setja sama restrictor og coursenumber",cast(new.restrictorID as char));
+		signal sqlstate '45000' set message_text = msg;
+    end if;
 end$$
 
 
 -- 2:
 -- Skrifið samskonar trigger fyrir update Restrictors skipunina.
+delimiter $$
+drop trigger if exists Restrict_Courses $$
 
+create trigger Restrict_Courses
+before update on restrictors
+for each row
+begin
+	declare msg varchar(255);
+    if(new.courseNumber = new.restrictorID)
+		then set msg = concat("Það er ekki hægt að setja sama restrictor og coursenumber",cast(new.restrictorID as char));
+		signal sqlstate '45000' set message_text = msg;
+    end if;
+end$$
 
 /*
 	3:
@@ -28,8 +43,25 @@ end$$
     Birta skal fullt nafn nemanda, heiti námsbrautar og fjölda lokinna eininga(
 	Aðeins skal velja staðinn áfanga. passed = true
 */
+delimiter $$
+drop procedure if exists TotalCredit $$
 
+create procedure TotalCredit(studentname varchar(50))
+begin
+select	concat(students.firstName," ",students.lastName) as FullName, 
+		tracks.trackName,
+		sum(courses.courseCredits) as TotalCredit
+        from registration 
+        join students on students.studentID = registration.studentID
+        join courses on courses.courseNumber = registration.courseNumber
+        join tracks on tracks.trackID = registration.trackID
+        where studentname = students.firstName && registration.passed = true;
+        
+end$$
 
+delimiter ;
+
+call TotalCredit('Guðrún');
 /*
 	4:
 	Skrifið 3 stored procedure-a:
@@ -39,4 +71,30 @@ end$$
     skylduáfanga á nemandann.
     Að endingu skrifið þið stored procedure-inn StudentRegistration() sem nota skal við sjálfstæða skráningu áfanga nemandans.
 */
+delimiter $$
 
+drop procedure if exists AddStudent $$
+create procedure AddStudent()
+begin
+end $$
+
+delimiter ;
+
+
+delimiter $$
+
+drop procedure if exists AddManditoryCourses $$
+create procedure AddManditoryCourses()
+begin
+end $$
+
+delimiter ;
+
+delimiter $$
+
+drop procedure if exists StudentRegistration $$
+create procedure StudentRegistration()
+begin
+end $$
+
+delimiter ;
